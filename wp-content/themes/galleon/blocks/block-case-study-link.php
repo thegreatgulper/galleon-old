bf7db450043a4f94d387cd885217d6baeb68a5d5
Tomasz <tomaszlipowicz@gmail.com>
2020-03-15 12:22:13 +0000

    test for blocks

--- wp-content/themes/galleon/blocks/block-case-study-link.php
+++ wp-content/themes/galleon/blocks/block-case-study-link.php
@@ -1,5 +1,7 @@
+<section class="case-study-link-block">
 <p class="button_link">
 	<a href="<?php block_field( 'link-url' ); ?>">
 		<?php block_field( 'link-label' ); ?> &gt;
 	</a>
 </p>
+</section>
